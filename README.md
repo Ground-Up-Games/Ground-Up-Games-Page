6515d012f565d0dde7969f6d1a5cfca5c2a23238
davica3 <davica3@D312-54576>
2021-06-21 12:33:35 +1200

    HTML 5 abscond and extricate deployment

--- README.md
+++
@@ -1,4 +0,0 @@
-# CADProgramming.github.io
-Software Engineering Portfolio
-
-Clayton Davidson (1000061387)
--- js/scroll_to_top.js
+++
@@ -1,5 +0,0 @@
-function scrollToTop() { 
-    window.scrollTo({ top: 0, behavior: 'smooth' }); 
-} 
-
-document.getElementById('scroll-return').addEventListener('click', scrollToTop);
